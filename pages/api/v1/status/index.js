cd05cb59f76a0aa587be69e43154c9764891a26f
Luiz Fernando Vital <luizfvital@gmail.com>
2024-04-17 17:33:43 +0000

    add postgres version, max_connections and active_connections to the `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,15 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  const res = await database.query(
+    `SELECT
+    (SELECT version()) AS postgres_version,
+    (SELECT setting FROM pg_settings WHERE name = 'max_connections') AS max_connections,
+    (SELECT count(*) FROM pg_stat_activity) AS active_connections;
+  `,
+  );
+
+  console.log(res.rows);
   const updatedAt = new Date().toISOString();
   response.status(200).json({
     updated_at: updatedAt,
